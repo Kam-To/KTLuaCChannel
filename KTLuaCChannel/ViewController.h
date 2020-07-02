ad00c449e0e2dcf6e4a3e112cb758b29f27307e2
Kam-To <kamtoto.au@gmail.com>
2020-07-02 20:47:57 +0800

    Initial Commit

---
+++ KTLuaCChannel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KTLuaCChannel
+//
+//  Created by cc on 2020/7/2.
+//  Copyright © 2020 kam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KTLuaCChannel/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  KTLuaCChannel
+//
+//  Created by cc on 2020/7/2.
+//  Copyright © 2020 kam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
